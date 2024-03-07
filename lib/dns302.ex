8cc3ae9ba6a10a4bff366219515b41326ab7a520
Andrew Fecheyr <andrew@bedesign.be>
2024-03-07 08:27:01 +0100

    mix new dns302 --sup

---
+++ lib/dns302.ex
@@ -0,0 +1,18 @@
+defmodule Dns302 do
+  @moduledoc """
+  Documentation for `Dns302`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dns302.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dns302_test.exs
@@ -0,0 +1,8 @@
+defmodule Dns302Test do
+  use ExUnit.Case
+  doctest Dns302
+
+  test "greets the world" do
+    assert Dns302.hello() == :world
+  end
+end
