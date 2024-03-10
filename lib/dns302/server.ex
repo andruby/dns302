71f1bb0a28a42cd20438c975c97a28e54336d317
Andrew Fecheyr <andrew@bedesign.be>
2024-03-10 07:50:58 +0100

    working redirecting server

--- lib/dns302/server.ex
+++ lib/dns302/server.ex
@@ -7,8 +7,20 @@ defmodule Dns302.Server do
   end
 
   def call(conn, _opts) do
+    case Dns302.get_redirect_url(conn.host) do
+      nil -> show_info_page(conn)
+      url -> redirect(conn, url)
+    end
+  end
+
+  def show_info_page(conn) do
     conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello world")
+    |> send_resp(200, "todo")
+  end
+
+  def redirect(conn, to) do
+    conn
+    |> put_resp_header("location", "#{to}")
+    |> send_resp(302, "You are being redirected")
   end
 end
