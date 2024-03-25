20c80ff0e9bdcdd6a4953c9f823827deec95925c
Andrew Fecheyr <andrew@bedesign.be>
2024-03-25 22:43:36 +0100

    Compile eex templates into functions
    Fixed production releases not finding the files

--- lib/dns302/server.ex
+++ lib/dns302/server.ex
@@ -1,19 +1,22 @@
 defmodule Dns302.Server do
   import Plug.Conn
+  require EEx
 
   @template_dir "lib/dns302/templates/"
+  EEx.function_from_file(:defp, :render_info, Path.join(@template_dir, "info.html.eex"), [])
+  EEx.function_from_file(:defp, :render_debug, Path.join(@template_dir, "debug.html.eex"), [:host])
 
   def init(options) do
     options
   end
 
   def call(%{host: host} = conn, [host: host]) do
-    render_template(conn, "info.html.eex")
+    send_resp(conn, 200, render_info())
   end
 
   def call(conn, _opts) do
     case Dns302.get_redirect_url(conn.host) do
-      nil -> show_debug_page(conn)
+      nil -> send_resp(conn, 200, render_debug(conn.host))
       url -> redirect(conn, url)
     end
   end
@@ -22,18 +25,5 @@ defmodule Dns302.Server do
     conn
     |> put_resp_header("location", "#{to}")
     |> send_resp(302, "You are being redirected")
-  end
-
-  defp show_debug_page(conn) do
-    render_template(conn, "debug.html.eex", host: conn.host)
-  end
-
-  defp render_template(%{status: status} = conn, template, assigns \\ []) do
-    body =
-      @template_dir
-      |> Path.join(template)
-      |> EEx.eval_file(assigns)
-
-    send_resp(conn, status || 200, body)
   end
 end
